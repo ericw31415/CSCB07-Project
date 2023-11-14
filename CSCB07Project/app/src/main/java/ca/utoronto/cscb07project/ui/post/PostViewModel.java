feec9d83ee681d9ca520b4b27cfb4f50a3bfd472
Eric Wan <ericwan957@gmail.com>
2023-11-14 03:36:36 -0500

    Add navigation bar

---
+++ CSCB07Project/app/src/main/java/ca/utoronto/cscb07project/ui/post/PostViewModel.java
@@ -0,0 +1,19 @@
+package ca.utoronto.cscb07project.ui.post;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PostViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public PostViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is POSt fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
