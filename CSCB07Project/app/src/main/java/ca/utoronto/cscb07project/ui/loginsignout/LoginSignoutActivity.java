21443135a206b8fef9eaf06a5ede280f102df158
calvin-liew <calvin.y.liew@gmail.com>
2023-11-22 12:11:11 -0500

    added toast message for user failure login and added UI fragmeent for logged in user in user activity

--- CSCB07Project/app/src/main/java/ca/utoronto/cscb07project/ui/loginsignout/LoginSignoutActivity.java
+++ CSCB07Project/app/src/main/java/ca/utoronto/cscb07project/ui/loginsignout/LoginSignoutActivity.java
@@ -1,34 +1,23 @@
 package ca.utoronto.cscb07project.ui.loginsignout;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 import ca.utoronto.cscb07project.R;
-import ca.utoronto.cscb07project.ui.signup.UserModel;
+import ca.utoronto.cscb07project.ui.user.UserActivity;
 
 public class LoginSignoutActivity extends AppCompatActivity implements LogInOutView{
 
     LoginPresenter presenter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +30,7 @@ public class LoginSignoutActivity extends AppCompatActivity implements LogInOutV
     private void loadFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.loginFrame, fragment);
+        transaction.replace(R.id.userFrame, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
@@ -58,34 +47,12 @@ public class LoginSignoutActivity extends AppCompatActivity implements LogInOutV
 
     @Override
     public void successfulLogin() {
-        Log.d("success", "logged in good");
-        loadFragment(new LoggedInFragment());
-
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference userRef = database.getReference("users").child(currentUser.getUid());
-
-        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    String firstName = dataSnapshot.child("fName").getValue(String.class);
-                    String lastName = dataSnapshot.child("lName").getValue(String.class);
-                    ((TextView)findViewById(R.id.greeting)).setText("Hello " + firstName + " " + lastName + "!");
-                    Log.d("User Data", "First Name: " + firstName + ", Last Name: " + lastName);
-                } else {
-                    Log.d("User Data", "DataSnapshot does not exist");
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.d("Error", "Database Error: " + error.getMessage());
-            }
-        });
+        Intent intent = new Intent(this, UserActivity.class);
+        startActivity(intent);
     }
     @Override
     public void unsuccessfulLogin() {
+        Toast.makeText(this, "Wrong email or password", Toast.LENGTH_SHORT).show();
         Log.d("failure", "logged in not good");
     }
 
---
+++ CSCB07Project/app/src/main/java/ca/utoronto/cscb07project/ui/user/UserActivity.java
@@ -0,0 +1,64 @@
+package ca.utoronto.cscb07project.ui.user;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import ca.utoronto.cscb07project.R;
+import ca.utoronto.cscb07project.ui.loginsignout.LoggedInFragment;
+
+public class UserActivity extends AppCompatActivity {
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.userFrame, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        Log.d("success", "logged in good");
+        loadFragment(new LoggedInFragment());
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = database.getReference("users").child(currentUser.getUid());
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String firstName = dataSnapshot.child("fName").getValue(String.class);
+                    String lastName = dataSnapshot.child("lName").getValue(String.class);
+                    ((TextView)findViewById(R.id.greeting)).setText("Hello " + firstName + " " + lastName + "!");
+                    Log.d("User Data", "First Name: " + firstName + ", Last Name: " + lastName);
+                } else {
+                    Log.d("User Data", "DataSnapshot does not exist");
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("Error", "Database Error: " + error.getMessage());
+            }
+        });
+    }
+}
