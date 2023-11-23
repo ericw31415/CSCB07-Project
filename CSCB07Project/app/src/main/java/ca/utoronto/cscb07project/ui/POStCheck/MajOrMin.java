c531dd10caa43c0b95de2a5632db46593e54ffc5
tamammakki <tamam.makki@mail.utoronto.ca>
2023-11-22 21:41:03 -0500

    User Story 2- created Minor check and POSt selection activities

---
+++ CSCB07Project/app/src/main/java/ca/utoronto/cscb07project/ui/POStCheck/MajOrMin.java
@@ -0,0 +1,16 @@
+package ca.utoronto.cscb07project.ui.POStCheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.utoronto.cscb07project.R;
+
+public class MajOrMin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maj_or_min);
+    }
+}
---
+++ CSCB07Project/app/src/main/java/ca/utoronto/cscb07project/ui/POStCheck/MinPOStCheck.java
@@ -0,0 +1,16 @@
+package ca.utoronto.cscb07project.ui.POStCheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.utoronto.cscb07project.R;
+
+public class MinPOStCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_min_post_check);
+    }
+}
