a567095eff80cbf3cdfac9fc87e1c9f22e8caf88
calvin-liew <calvin.y.liew@gmail.com>
2023-12-02 21:50:55 -0500

    Add new event selection

--- CSCB07Project/app/src/main/java/ca/utoronto/cscb07project/ui/user/AddAnnouncmentFragment.java
+++ CSCB07Project/app/src/main/java/ca/utoronto/cscb07project/ui/user/AddAnnouncmentFragment.java
@@ -5,20 +5,17 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -38,7 +35,6 @@ import ca.utoronto.cscb07project.announcements.Announcement;
 import ca.utoronto.cscb07project.events.Event;
 import ca.utoronto.cscb07project.events.EventAdapter;
 
-
 public class AddAnnouncmentFragment extends Fragment {
 
     private EditText titleEditText;
@@ -49,7 +45,6 @@ public class AddAnnouncmentFragment extends Fragment {
     private List<Event> eventsList;
 
     private DatabaseReference eventsRef;
-
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -65,12 +60,12 @@ public class AddAnnouncmentFragment extends Fragment {
         eventsRecyclerView = view.findViewById(R.id.recyclerViewEvent);
         eventsList = new ArrayList<>();
         eventAdapter = new EventAdapter(requireContext(), R.layout.event_item, eventsList);
+        eventsRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
         eventsRecyclerView.setAdapter(eventAdapter);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         eventsRef = database.getReference("Events");
         fetchEventsFromFirebase();
-
 
         // Set up the submit button click listener
         submitButton.setOnClickListener(v -> postAnnouncement());
@@ -107,7 +102,6 @@ public class AddAnnouncmentFragment extends Fragment {
         });
     }
 
-
     private void postAnnouncement() {
         // Collect input data
         String title = titleEditText.getText().toString().trim();
@@ -119,25 +113,21 @@ public class AddAnnouncmentFragment extends Fragment {
             return;
         }
 
-
         Date currentDate = new Date();
-
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
-                Locale.getDefault());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
         String formattedDate = dateFormat.format(currentDate);
 
         // Submit the complaint to Firebase
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference announcementsRef = database.getReference("Announcements");
         String AnnouncementId = announcementsRef.push().getKey();
+
         if (AnnouncementId != null) {
             Announcement announcement = new Announcement(title, formattedDate, details, AnnouncementId);
             announcementsRef.child(AnnouncementId).setValue(announcement)
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
                             Toast.makeText(getContext(), "Announcement posted", Toast.LENGTH_SHORT).show();
-
                             sendPushNotification(title, details);
                             getParentFragmentManager().popBackStack();
                         } else {
@@ -169,61 +159,5 @@ public class AddAnnouncmentFragment extends Fragment {
         // Display the notification
         NotificationManager notificationManager = (NotificationManager) requireContext().getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(1, builder.build());
-    }
-
-
-    /**
-     * A simple {@link Fragment} subclass.
-     * Use the {@link AnnouncmenetDetailFragment#newInstance} factory method to
-     * create an instance of this fragment.
-     */
-    public static class AnnouncmenetDetailFragment extends Fragment {
-
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private static final String ARG_PARAM1 = "param1";
-        private static final String ARG_PARAM2 = "param2";
-
-        // TODO: Rename and change types of parameters
-        private String mParam1;
-        private String mParam2;
-
-        public AnnouncmenetDetailFragment() {
-            // Required empty public constructor
-        }
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AnnouncmenetDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        public static AnnouncmenetDetailFragment newInstance(String param1, String param2) {
-            AnnouncmenetDetailFragment fragment = new AnnouncmenetDetailFragment();
-            Bundle args = new Bundle();
-            args.putString(ARG_PARAM1, param1);
-            args.putString(ARG_PARAM2, param2);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            if (getArguments() != null) {
-                mParam1 = getArguments().getString(ARG_PARAM1);
-                mParam2 = getArguments().getString(ARG_PARAM2);
-            }
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_announcement__detail, container, false);
-        }
     }
 }
