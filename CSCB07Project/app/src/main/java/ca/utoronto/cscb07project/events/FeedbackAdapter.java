fff5b1dfe326a0ab3e30b9e0eacf68b645526d4d
tamammakki <tamam.makki@mail.utoronto.ca>
2023-12-03 19:31:11 -0500

    Improved cardview for Admin viewing user reviews

--- CSCB07Project/app/src/main/java/ca/utoronto/cscb07project/events/FeedbackAdapter.java
+++ CSCB07Project/app/src/main/java/ca/utoronto/cscb07project/events/FeedbackAdapter.java
@@ -1,6 +1,7 @@
 package ca.utoronto.cscb07project.events;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,6 +39,15 @@ public class FeedbackAdapter extends RecyclerView.Adapter<FeedbackAdapter.EventV
     public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
         Review currentRev = reviews.get(position);
         holder.bind(currentRev, listener);
+
+        //added
+        /**
+        if (TextUtils.isEmpty(currentRev.getReviewText())) {
+            holder.reviewText.setText("No response");
+        } else {
+            holder.reviewText.setText(currentRev.getReviewText());
+        }
+         */
     }
 
     @Override
@@ -55,21 +65,27 @@ public class FeedbackAdapter extends RecyclerView.Adapter<FeedbackAdapter.EventV
         private TextView rating;
         private TextView reviewText;
 
-        private TextView eventId;
+        //private TextView eventId;
 
         public EventViewHolder(@NonNull View itemView) {
             super(itemView);
             userEmail = itemView.findViewById(R.id.userEmail);
             rating = itemView.findViewById(R.id.rating);
             reviewText = itemView.findViewById(R.id.reviewText);
-            eventId = itemView.findViewById(R.id.eventId);
+            //eventId = itemView.findViewById(R.id.eventId);
         }
 
         public void bind(final Review review, final OnItemClickListener listener) {
             userEmail.setText(review.getUserEmail());
             rating.setText(String.valueOf(review.getRating())); // Convert rating to String
-            reviewText.setText(review.getReviewText());
-            eventId.setText(review.getEventId());
+            //reviewText.setText(review.getReviewText());
+            //eventId.setText(review.getEventId());
+            // Set text comment or "No response" if it's empty
+            if (TextUtils.isEmpty(review.getReviewText())) {
+                reviewText.setText("No response");
+            } else {
+                reviewText.setText(review.getReviewText());
+            }
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
